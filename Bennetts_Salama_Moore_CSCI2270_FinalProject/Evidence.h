92e4bb154b7e204f11e6408d1f954e3b5b41ede5
user <samuel.bennetts@colorado.edu>
2016-04-21 14:09:52 -0600

    modified files

---
+++ Bennetts_Salama_Moore_CSCI2270_FinalProject/Evidence.h
@@ -0,0 +1,54 @@
+#ifndef EVIDENCE_H
+#define EVIDENCE_H
+
+struct EvidenceNode{
+    std::string itemName; //type of evidence, eg. drugs/guns
+    int caseNum; //case number attached to evidence.
+    int quantity; //amount of evidence attached to individual person
+    std::string renterName; //name of renter
+    int shelfNumber; //shelf number
+    EvidenceNode *parent;
+    EvidenceNode *leftChild;
+    EvidenceNode *rightChild;
+
+    EvidenceNode(){};
+
+    EvidenceNode(int in_caseNum, std::string in_itemName, int in_shelfNumber, int in_quantity)
+    {
+        itemName = in_itemName;
+        caseNum = in_caseNum;
+        quantity = in_quantity;
+        shelfNumber = in_shelfNumber;
+        renterName = NULL;
+        parent = NULL;
+        leftChild = NULL;
+        rightChild = NULL;
+    }
+};
+
+class Evidence
+{
+    public:
+        EvidenceNode();
+        ~EvidenceNode();
+        void buildEvidenceLog(char *filename);
+        void printEvidenceInventory();
+        int countEvidenceNodes();
+        void deleteEvidenceNode(std::string title);
+        void addEvidenceNode(int in_caseNum, std::string in_itemName, int in_shelfNumber, int in_quantity); //does not include renter name.
+        void findEvidence(int caseNum); //currently only searches for caseNum
+        void rentEvidence(int caseNum, std::string in_renterName);
+        void returnEvidence(int caseNum) //sets renterName to null
+
+    protected:
+
+    private:
+        void DeleteAll(EvidenceNode * node); //use this for the post-order traversal deletion of the tree
+        void printEvidenceInventory(EvidenceNode * node);
+        int countEvidenceNodes(EvidenceNode *node);
+        EvidenceNode* search(EvidenceNode *search,int in_caseNum);
+        EvidenceNode* EvidenceMinimum(EvidenceNode *node);
+        EvidenceNode *root;
+};
+
+#endif // EVIDENCETREE_H
